dc6cb14a806947ac1d022f77659533bd64cfbf0d
Ledesma-Lautaro <ledesmalautaroezequiel02@gmail.com>
2024-05-08 16:23:15 -0300

    added cards functionality(copy, rename and delete) continue with favorites cards

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://warm-duckling-93.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ convex/board.ts
@@ -0,0 +1,80 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+import { error } from "console";
+
+const images = [
+    "/placeholders/1.svg",
+    "/placeholders/2.svg",
+    "/placeholders/3.svg",
+    "/placeholders/4.svg",
+    "/placeholders/5.svg",
+    "/placeholders/6.svg",
+    "/placeholders/7.svg",
+    "/placeholders/8.svg",
+    "/placeholders/9.svg",
+    "/placeholders/10.svg",
+]
+
+export const create = mutation ({
+    args : {
+        orgId: v.string(),
+        title: v.string(),
+    },
+    handler: async(ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity){
+            throw new Error("Unauthorized");
+        }
+
+        const randomImage = images[Math.floor(Math.random() * images.length)];
+        const board = await ctx.db.insert("boards", {
+            title: args.title,
+            orgId: args.orgId,
+            authorId: identity.subject,
+            authorName: identity.name ?? "", // Add nullish coalescing operator to provide a default value
+            imageUrl: randomImage,
+        });
+
+        return board;
+    
+    }
+})
+
+export const remove = mutation({
+    args: {id: v.id("boards")},
+    handler: async (ctx,args) =>{
+        const identity = await ctx.auth.getUserIdentity();
+    
+        if(!identity){
+            throw new Error("Unauthorized");
+        }
+
+        await ctx.db.delete(args.id);
+    }
+})
+
+export const update = mutation({
+    args: {id:v.id("boards"), title: v.string()},
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity){
+            throw new Error("Unauthorized");
+        }
+
+        const title = args.title.trim();
+        if(!title){
+            throw new Error("Title cannot be empty");
+        }
+        if(title.length > 60){
+            throw new Error("Title cannot be longer than 60 characters");
+        }
+
+        const board = await ctx.db.patch(args.id,{
+            title:args.title,
+        });
+        
+        return board;
+    }
+})
